69404a8f7b2bf943d34933dc22e28d8c226f28b0
gizmore <gizmore@wechall.net>
2023-02-02 18:17:43 +0100

    '7.0.1-r1725 avatar gd dependency'

--- Module_Avatar.php
+++ Module_Avatar.php
@@ -14,60 +14,119 @@ use GDO\UI\GDT_Bar;
  * Avatar module.
  * Features default avatars.
  * Avatars have a colored border depending on the two real genders: mandalorian and apache helicopter.
- * 
+ * This module requires the php gd library.
+ *
  * @author gizmore
- * @version 7.0.1
+ * @version 7.0.2
  * @since 6.2.0
  * @see GDT_ImageFile
  */
 final class Module_Avatar extends GDO_Module
 {
-	##############
-	### Module ###
-	##############
-	public function getClasses() : array { return ['GDO\Avatar\GDO_Avatar','GDO\Avatar\GDO_UserAvatar']; }
-	public function getDependencies() : array { return ['File']; }
-	public function getFriendencies() : array { return ['Account']; }
-	public function onLoadLanguage() : void { $this->loadLanguage('lang/avatar'); }
-	public function onIncludeScripts() : void { $this->addCSS('css/gdo-avatar.css'); }
 
-	##############
-	### Config ###
-	##############
-	public function getConfig() : array
+	# #############
+	# ## Module ###
+	# #############
+	public function getClasses(): array
 	{
 		return [
-		    GDT_Checkbox::make('avatar_guests')->initial('1'),
-		    GDT_Checkbox::make('hook_sidebar')->initial('1'),
-		    GDT_ImageFile::make('avatar_image_guest')->previewHREF(href('Avatar', 'Image', '&file={id}'))->scaledVersion('icon', 96, 96)->scaledVersion('thumb', 375, 375),
-		    GDT_ImageFile::make('avatar_image_member')->previewHREF(href('Avatar', 'Image', '&file={id}'))->scaledVersion('icon', 96, 96)->scaledVersion('thumb', 375, 375),
-		    GDT_ImageFile::make('avatar_image_male')->previewHREF(href('Avatar', 'Image', '&file={id}'))->scaledVersion('icon', 96, 96)->scaledVersion('thumb', 375, 375),
-		    GDT_ImageFile::make('avatar_image_female')->previewHREF(href('Avatar', 'Image', '&file={id}'))->scaledVersion('icon', 96, 96)->scaledVersion('thumb', 375, 375),
+			GDO_Avatar::class,
+			GDO_UserAvatar::class
 		];
 	}
-	public function cfgGuestAvatars() { return $this->getConfigValue('avatar_guests'); }
-	public function cfgSidebar() { return $this->getConfigValue('hook_sidebar'); }
-	public function cfgColAvatarGuest() : GDT_ImageFile { return $this->getConfigColumn('avatar_image_guest'); }
-	
-	############
-	### Init ###
-	############
-	public function onInitSidebar() : void
+
+	public function getDependencies(): array
 	{
-	    if ($this->cfgSidebar())
-	    {
-	        GDT_Page::$INSTANCE->leftBar()->addField(
-	            GDT_Link::make()->text('mt_avatar_gallery')->href(
-	                href('Avatar', 'Gallery')));
-	    }
+		return [
+			'File'
+		];
 	}
-	
-	###############
-	### Install ###
-	###############
-	public function onInstall() : void
+
+	public function getFriendencies(): array
 	{
-		if (!($image = $this->getConfigValue('avatar_image_guest')))
+		return [
+			'Account'
+		];
+	}
+
+	public function onLoadLanguage(): void
+	{
+		$this->loadLanguage('lang/avatar');
+	}
+
+	public function onIncludeScripts(): void
+	{
+		$this->addCSS('css/gdo-avatar.css');
+	}
+
+	# #############
+	# ## Config ###
+	# #############
+	public function getConfig(): array
+	{
+		return [
+			GDT_Checkbox::make('avatar_guests')->initial('1'),
+			GDT_Checkbox::make('hook_sidebar')->initial('1'),
+			GDT_ImageFile::make('avatar_image_guest')->previewHREF(href('Avatar', 'Image', '&file={id}'))
+				->scaledVersion('icon', 96, 96)
+				->scaledVersion('thumb', 375, 375),
+			GDT_ImageFile::make('avatar_image_member')->previewHREF(href('Avatar', 'Image', '&file={id}'))
+				->scaledVersion('icon', 96, 96)
+				->scaledVersion('thumb', 375, 375),
+			GDT_ImageFile::make('avatar_image_male')->previewHREF(href('Avatar', 'Image', '&file={id}'))
+				->scaledVersion('icon', 96, 96)
+				->scaledVersion('thumb', 375, 375),
+			GDT_ImageFile::make('avatar_image_female')->previewHREF(href('Avatar', 'Image', '&file={id}'))
+				->scaledVersion('icon', 96, 96)
+				->scaledVersion('thumb', 375, 375),
+		];
+	}
+
+	public function cfgGuestAvatars()
+	{
+		return $this->getConfigValue('avatar_guests');
+	}
+
+	public function cfgSidebar()
+	{
+		return $this->getConfigValue('hook_sidebar');
+	}
+
+	public function cfgColAvatarGuest(): GDT_ImageFile
+	{
+		return $this->getConfigColumn('avatar_image_guest');
+	}
+
+	# ###########
+	# ## Init ###
+	# ###########
+	public function onInitSidebar(): void
+	{
+		if ($this->cfgSidebar())
+		{
+			GDT_Page::$INSTANCE->leftBar()->addField(
+				GDT_Link::make()->text('mt_avatar_gallery')
+					->href(href('Avatar', 'Gallery')));
+		}
+	}
+
+	# ##############
+	# ## Install ###
+	# ##############
+	public function checkSystemDependencies(): bool
+	{
+		if ( !function_exists('imagecreatefromjpeg'))
+		{
+			return $this->errorSystemDependency('err_php_extension', [
+				'gd2'
+			]);
+		}
+		return true;
+	}
+
+	public function onInstall(): void
+	{
+		if ( !($image = $this->getConfigValue('avatar_image_guest')))
 		{
 			$path = $this->filePath('tpl/img/default.jpeg');
 			$image = GDO_File::fromPath('default.jpeg', $path)->insert();
@@ -76,25 +135,18 @@ final class Module_Avatar extends GDO_Module
 			$this->saveConfigVar('avatar_image_guest', $image->getID());
 		}
 	}
-	
-	############
-	### Hook ###
-	############
-	public function hookAccountChanged(GDO_User $user) : void
+
+	# ###########
+	# ## Hook ###
+	# ###########
+	public function hookAccountChanged(GDO_User $user): void
 	{
 		$user->tempUnset('gdo_avatar');
 	}
 
-	public function hookAccountBar(GDT_Bar $bar) : void
+	public function hookAccountBar(GDT_Bar $bar): void
 	{
 		$bar->addField(GDT_Link::make('btn_avatar')->href(href('Avatar', 'Set')));
 	}
-
-// 	public function hookCreateCardUserProfile(GDT_Card $card)
-// 	{
-// 		$user = $card->gdo->getUser();
-// 		$avatar = GDT_Avatar::make()->user($user);
-// 		$card->addFieldFirst($avatar);
-// 	}
 
 }
