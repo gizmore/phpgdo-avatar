9103488c1e3d6150ae9eeb9f62cd2af423b186d1
gizmore <gizmore@wechall.net>
2026-07-15 20:52:32 +0200

    'fixes'

--- Method/Upload.php
+++ Method/Upload.php
@@ -40,7 +40,7 @@ final class Upload extends MethodForm
 	{
 		$form->addField(GDO_Avatar::forUser(GDO_User::current())->gdoColumn('avatar_file_id')->action($this->href()));
 		$form->actions()->addField(GDT_Submit::make()->label('btn_upload'));
-		$form->addField(GDT_AntiCSRF::make());
+		$form->addField(GDT_AntiCSRF::make()->fixed());
 		$form->actions()->addField(GDT_Button::make('btn_set_avatar')->href(href('Avatar', 'Set')));
 	}
 
