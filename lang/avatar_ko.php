ccece5fd81d7683530e5f84a9ac9e7bf53d29c3b
gizmore <gizmore@wechall.net>
2026-09-18 04:06:19 +0200

    'more korean'

---
+++ lang/avatar_ko.php
@@ -0,0 +1,23 @@
+<?php
+namespace GDO\Avatar\lang;
+
+return [
+	'btn_avatar' => '아바타',
+	'btn_set_avatar' => '아바타 설정',
+	'avatar_of' => '%s의 아바타',
+	'cfg_avatar_guests' => '게스트 아바타를 허용하시겠습니까?',
+	'cfg_avatar_image_guest' => '기본 게스트 아바타',
+	'cfg_avatar_image_member' => '기본 회원 아바타',
+	'cfg_avatar_image_male' => '기본 남성 아바타',
+	'cfg_avatar_image_female' => '기본 여성 아바타',
+	'mt_avatar_set' => '아바타 설정',
+	'avatar' => '아바타',
+	'no_avatar' => '아바타 없음',
+	'msg_avatar_set' => '귀하의 아바타 이미지가 선택되었습니다.',
+	'choice_no_avatar' => '아바타 없음',
+	'mt_avatar_upload' => '아바타 업로드',
+	'msg_avatar_uploaded' => '귀하의 아바타 이미지가 업로드되어 선택되었습니다.',
+	'mt_avatar_image' => '아바타 이미지',
+	'mt_avatar_gallery' => '아바타 갤러리',
+	'list_avatar_gallery' => '%s 사용자 아바타',
+];
